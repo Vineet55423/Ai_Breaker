58acfd4c4f632934b7acd78706b7c220e0747792
Vineet55423 <vineetyadav55423@gmail.com>
2025-11-16 08:42:06 +0530

    Intergration + Auth

--- database.js
+++ database.js
@@ -1,48 +1,93 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app-compat.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics-compat.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase Authentication:
 
-// Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD3YmYxGImkv1lzQU9bVy9E8gC-l0JSsag",
-    authDomain: "ai-breaker-s.firebaseapp.com",
-    databaseURL: "https://ai-breaker-s-default-rtdb.firebaseio.com",
-    projectId: "ai-breaker-s",
-    storageBucket: "ai-breaker-s.firebasestorage.app",
-    messagingSenderId: "632955331376",
-    appId: "1:632955331376:web:22cb4ee9fd706796309ef5",
-    measurementId: "G-2XW8XMYMX3"
+  apiKey: "AIzaSyD3YmYxGImkv1lzQU9bVy9E8gC-l0JSsag",
+  authDomain: "ai-breaker-s.firebaseapp.com",
+  databaseURL: "https://ai-breaker-s-default-rtdb.firebaseio.com",
+  projectId: "ai-breaker-s",
+  storageBucket: "ai-breaker-s.firebasestorage.app",
+  messagingSenderId: "632955331376",
+  appId: "1:632955331376:web:22cb4ee9fd706796309ef5",
+  measurementId: "G-2XW8XMYMX3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = firebase.auth();
-const db = firebase.database();
+const auth = getAuth(app);
 
-document.getElementById("signupForm").addEventListener("submit", (e) => {
+// window.auth = auth;
+
+//input
+
+// submit button
+// const logSubmit = document.getElementById('logSubmit');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const logSubmit = document.getElementById('logButton');
+
+    logSubmit.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                window.location.href = "main.html";
+                console.log("Account Created....")
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+    });
+
+});
+
+
+// Firebase Realtime Database :
+
+import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+const db = getDatabase(app);
+
+// Form Submit Listener
+document.getElementById('signupForm').addEventListener('submit', submitform);
+
+// Submit Function
+function submitform(e) {
     e.preventDefault();
 
-    let email = document.getElementById("email").value;
-    let confirmPassword = document.getElementById("signup-confirm-password").value;
-    let password = document.getElementById("signup-password").value;
+    const signupName = getElementVal('signup-name');
+    const signupEmail = getElementVal('signup-email');
+    const signupPass = getElementVal('signup-password');
+    const signupConfirmPass = getElementVal('signup-confirm-password');
+    
+    saveMess(signupName, signupEmail, signupPass, signupConfirmPass);
+}
 
+// Save Message to Firebase
+function saveMess(signupName, signupEmail, signupPass, signupConfirmPass) {
+    const myprojectRef = ref(db, "signupForm/");
+    const newPostRef = push(myprojectRef);
 
-    // Save login data in database
-    db.ref("files/").push({
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword,
-        time: new Date().toString()
-    })
-        .then(() => {
-            alert("Data Saved!");
-        })
-        .catch((err) => {
-            alert("Error: " + err);
-        });
-});
+    set(newPostRef, {
+        signupName: signupName,
+        signupEmail: signupEmail,
+        signupPass: signupPass,
+        signupConfirmPass: signupConfirmPass
+    });
+}
+
+// Helper Function
+function getElementVal(id) {
+    return document.getElementById(id).value;
+}
